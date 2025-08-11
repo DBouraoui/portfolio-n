57da862eeecc745e98e30d939b1620bf714c36ba
DBouraoui <contact@dbouraoui.fr>
2025-08-11 19:52:10 +0200

    minor: add tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui'],
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
   app: {
     head: {
       title: 'Mon Portfolio - Développeur Web',
